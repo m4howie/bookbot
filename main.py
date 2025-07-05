e41b470eeb3bdda7e4c5ca8ea329b8782f187681
m4howie <m4howie@hotmail.com>
2025-07-05 10:50:28 -0500

    ch2/L6 count characters stats.py and main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from stats import get_num_words
-
+from stats import count_letters
 def main():
     path = "/home/m4howie/bookbot/books/frankenstein.txt"
     text = get_book_text(path)
     num_words = get_num_words(text)
-    print(f"({num_words} words found in the document")
+    book_dict = count_letters(text)
+    print(f"{num_words} words found in the document", book_dict)
 
 
 
--- stats.py
+++ stats.py
@@ -1,3 +1,14 @@
 def get_num_words(text):
     words = text.split()
     return len(words)
+
+def count_letters(text):
+    book_dict = {}
+    text = text.lower()
+    for letters in text:
+        if letters in book_dict:
+            book_dict[letters] += 1
+        else:
+            book_dict[letters] = 1
+
+    return book_dict

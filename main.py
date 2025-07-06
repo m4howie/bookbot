f067225ef0d53776e05e2e0a8380f84dabfa669b
m4howie <m4howie@hotmail.com>
2025-07-06 01:53:26 -0500

    ch3/l1 Print Report

--- main.py
+++ main.py
@@ -1,17 +1,33 @@
 from stats import get_num_words
 from stats import count_letters
+from stats import chars_dict_to_sort_list
+
 def main():
     path = "/home/m4howie/bookbot/books/frankenstein.txt"
     text = get_book_text(path)
     num_words = get_num_words(text)
     book_dict = count_letters(text)
-    print(f"{num_words} words found in the document", book_dict)
+    chars_sorted_list = chars_dict_to_sort_list(book_dict)
+    print_report(path, num_words, chars_sorted_list)
 
 
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+
+def print_report(path, num_words, chars_sorted_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item['char']}: {item['num']}")
+
+    print("============= END ===============")
 
 
 main()
--- stats.py
+++ stats.py
@@ -12,3 +12,13 @@ def count_letters(text):
             book_dict[letters] = 1
 
     return book_dict
+
+def sort_on(d):
+    return d["num"]
+
+def chars_dict_to_sort_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
